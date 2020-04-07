9734ad0250a8357034bfe0fc69956f4d43c8606a
Dustin Milam <darklordsushi@yahoo.com>
2020-04-07 10:52:36 -0400

    add webhook to restock refunds

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,6 +1,6 @@
 class OrderController < ApplicationController
 
-  skip_before_filter :verify_authenticity_token, :only => [:receive]
+  skip_before_filter :verify_authenticity_token, :only => [:receive, :refund]
 
   def receive
     puts params
@@ -35,5 +35,41 @@ class OrderController < ApplicationController
 
     head :ok
   end 
+
+  def refund
+    puts params
+
+    for refund_line_item in params["refund_line_items"]
+      if refund_line_item["line_item"]["title"].include? "(Kit)"
+        line_item = refund_line_item["line_item"]
+
+        begin
+          counterpart_title = line_item["title"].gsub("(Kit)",'').strip
+          main_product = ShopifyAPI::Product.find(:all, params: {title: counterpart_title}).select{|p| p.title == counterpart_title}&.first
+
+          puts Colorize.magenta(main_product.title)
+
+          variant = main_product.variants.select{|v| v.title == line_item["variant_title"]}.first
+          inventory_item_params = { inventory_item_ids: variant.inventory_item_id }
+          inventory_levels = ShopifyAPI::InventoryLevel.find(:all, params: inventory_item_params)
+
+          puts Colorize.magenta(variant.id)
+
+          if inventory_levels[0].adjust(line_item["quantity"].to_i)
+            puts Colorize.green("Restocked inventory by #{line_item["quantity"]}")
+          else
+            puts Colorize.red("error setting quantity")
+            puts inventory_levels[0]
+          end
+        rescue Exception => e
+          puts Colorize.red(e)
+        end
+      else
+        puts Colorize.cyan("Not a kit item")
+      end
+    end
+
+    head :ok
+  end
 
 end
